b547e435a4ee89be28c72e4ead484c08bbd065bd
pchira@upscalesoft.in <pchira@upscalesoft.com>
2019-05-29 12:55:04 +0530

    search functionaliy added

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 console.log('I think its working')
 
 
-var array = ["google","firefox","chrome","internet explorer"]
+var array = ["google","firefox","firelone","chrome","internet explorer"]
+var submit = document.querySelector('#submit');
+var resultantArray = [];
 
-
+var search = document.querySelector('#search');
 
 var input = "fire"
 
 function getResult(input){
-
+    console.log(input)
     array.forEach(cur=> {
         if(cur.includes(input)){
-            console.log(cur)
+            resultantArray.push(cur);
         }
     });
 
 }
 
-getResult(input);
+    
+
+    submit.addEventListener('click',()=>{
+        getResult(search.value);  
+        for(var i=0;i<resultantArray.length;i++){
+            var node = document.createElement('li');
+    
+            var text = document.createTextNode(resultantArray[i]);
+        
+            node.appendChild(text);
+            document.querySelector('#list').appendChild(node)
+
+        }
+      
+    })
+
